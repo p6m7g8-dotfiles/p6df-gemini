473f71a85192ec06f79981e4892e7a477a239bf8
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 18:55:43 -0500

    feat: v1

--- init.zsh
+++ init.zsh
@@ -8,17 +8,34 @@
 ######################################################################
 p6df::modules::gemini::deps() {
   ModuleDeps=(
+    p6m7g8-dotfiles/p6common
   )
 }
 
 ######################################################################
 #<
 #
-# Function: p6df::modules::gemini::external::brew()
+# Function: p6df::modules::gemini::vscodes()
 #
 #>
 ######################################################################
-p6df::modules::gemini::external::brew() {
+p6df::modules::gemini::vscodes() {
+
+  code --install-extension google.gemini-code-assist
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gemini::external::brews()
+#
+#>
+######################################################################
+p6df::modules::gemini::external::brews() {
+
+  p6df::modules::homebrew::cli::brew::install gemini-cli
 
   p6_return_void
 }
@@ -28,9 +45,39 @@ p6df::modules::gemini::external::brew() {
 #
 # Function: p6df::modules::gemini::home::symlink()
 #
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
 #>
 ######################################################################
 p6df::modules::gemini::home::symlink() {
+
+  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-gemini/share/gemini" .gemini
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gemini::langs(_dir)
+#
+#  Args:
+#	_dir -
+#
+#>
+######################################################################
+p6df::modules::gemini::langs() {
+  local _dir="$1"
+
+  gemini extensions install --consent https://github.com/gemini-cli-extensions/workspace
+  gemini extensions enable google-workspace
+  gemini extensions install --consent https://github.com/github/github-mcp-server
+  gemini extensions install --consent https://github.com/gemini-cli-extensions/security
+  gemini extensions install --consent https://github.com/gemini-cli-extensions/code-review
+  gemini extensions install --consent https://github.com/ox01024/gemini-cli-git
+  gemini extensions install --consent https://github.com/saadmanrafat/uv-mcp
+  gemini extensions install --consent https://github.com/Menghuan1918/gemini-cli-gopls
+  gemini extensions install --consent https://github.com/gemini-cli-extensions/conductor # --auto-update
+  gemini extensions install --consent https://github.com/DoIT-Artificial-Intelligence/youtube-to-docs.git
 
   p6_return_void
 }
@@ -50,29 +97,22 @@ p6df::modules::gemini::aliases::init() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::gemini::prompt::mod()
-#
-#>
-######################################################################
-p6df::modules::gemini::prompt::mod() {
-
-  p6_gemini_prompt_info
-}
-
-######################################################################
-#<
-#
-# Function: str str = p6_gemini_prompt_info()
+# Function: str str = p6df::modules::gemini::prompt::mod()
 #
 #  Returns:
 #	str - str
 #
+#  Environment:	 HOME
 #>
 ######################################################################
-p6_gemini_prompt_info() {
+p6df::modules::gemini::prompt::mod() {
 
-  local str
-  str="gemini:\t\t  "
+  local user=$(cat "$HOME"/.gemini/google_accounts.json | jq  -r '.active')
+
+  local str 
+  if ! p6_string_blank "$user"; then
+    str="gemini:\t\t  $user"
+  fi
 
   p6_return_str "$str"
 }
